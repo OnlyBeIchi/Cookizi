520bf1a214d7330f2fe991177428b6a66f4ebd57
OnlybeIchi <Admin@DESKTOP-78K56LL>
2022-05-23 20:13:52 +0700

    Test Commit

---
+++ Controllers/FoodsController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Cookizi.Controllers
+{
+    public class FoodsController : Controller
+    {
+        // GET: Foods
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
