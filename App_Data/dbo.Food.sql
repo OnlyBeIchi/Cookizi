1db3ccf5739dd3e6c7060a748269fa43ca22c5ae
nmp2002 <106230025+nmp2002@users.noreply.github.com>
2022-06-19 17:24:33 +0700

    Add files via upload

---
+++ App_Data/dbo.Food.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Food] (
+    [Id]         INT            IDENTITY (1, 1) NOT NULL,
+    [TenFood]    NVARCHAR (MAX) NULL,
+    [NguyenLieu] NVARCHAR (MAX) NULL,
+    [Calories] INT NOT NULL, 
+    PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
