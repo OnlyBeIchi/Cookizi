4e769dcc5b11f0134012b288e9d6bf5199adc8cc
nmp2002 <106230025+nmp2002@users.noreply.github.com>
2022-06-19 17:26:18 +0700

    Add files via upload

--- Models/QLFood.cs
+++ Models/QLFood.cs
@@ -17,6 +17,11 @@ namespace Cookizi.Models
         [Required(ErrorMessage = "Bạn cần nhập vào nguyên liệu")]
         [Display(Name = "Nguyên Liệu")]
         public string NguyenLieu { set; get; }
+        public int Calories { set; get; }
+       
+        [Required(ErrorMessage = "Bạn cần nhập vào công thức nấu ăn")]
+        [Display(Name = "Công thức nấu ăn")]
+        public string Congthucnauan { set; get; }
     }
 
     class FoodList
@@ -57,8 +62,8 @@ namespace Cookizi.Models
                 strFood.ID = Convert.ToInt32(dt.Rows[i]["Id"].ToString());
                 strFood.TenFood = dt.Rows[i]["TenFood"].ToString();
                 strFood.NguyenLieu = dt.Rows[i]["NguyenLieu"].ToString();
-                
-
+                strFood.Congthucnauan = dt.Rows[i]["Congthucnauan"].ToString();
+                strFood.Calories =int.Parse(dt.Rows[i]["Calories"].ToString());
                 strList.Add(strFood);
             }
             return strList;
@@ -67,7 +72,7 @@ namespace Cookizi.Models
         // Thêm dữ liệu
         public void AddFood(QLFood strFood)
         {
-            string sql = "INSERT INTO Food(TenFood, NguyenLieu)VALUES(N'" +strFood.TenFood+ "', N'" + strFood.NguyenLieu + "')";
+            string sql = "INSERT INTO Food(TenFood, NguyenLieu,Calories,Congthucnauan)VALUES(N'" +strFood.TenFood+ "', N'" + strFood.NguyenLieu + "', N'" + strFood.Congthucnauan + "', N'" + strFood.Calories +"')";
             SqlConnection con = db.GetConnection();
             SqlCommand cmd = new SqlCommand(sql, con);
 
@@ -82,7 +87,7 @@ namespace Cookizi.Models
         // Sửa dữ liệu
         public void EditFood(QLFood strFood)
         {
-            string sql = "UPDATE Food SET TenFood = N'" + strFood.TenFood + "',NguyenLieu =  N'" + strFood.NguyenLieu + "' WHERE Id =" + strFood.ID;
+            string sql = "UPDATE Food SET TenFood = N'" + strFood.TenFood + "',NguyenLieu =  N'" + strFood.NguyenLieu + "', N'" + strFood.Congthucnauan + "' WHERE Id =" + strFood.ID;
             SqlConnection con = db.GetConnection();
             SqlCommand cmd = new SqlCommand(sql, con);
 
